59108c8206851cb9e624120ee6324bb8b21db651
xaphaniafox <f.hosseini64@gmail.com>
2023-04-04 09:58:47 +0300

    adding Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './style.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Card from './components/Card'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <Hero />
+      <Card />
     </>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+const Card = () => {
+  return (
+    <div className="card">
+      <img src='src/images/katie-zaferes.png' className="card--image" />
+      <div className="card--stats">
+        <img src='src/images/star.png' className="card-star" />
+        <span>5.0 </span>
+        <span className="gray">(6) . </span>
+        <span className="gray">USA</span>
+      </div>
+      <p>Life lessons with Katie Zaferes</p>
+      <p><span className="bold">From $136</span> / person</p>
+    </div>
+  )
+}
+export default Card
