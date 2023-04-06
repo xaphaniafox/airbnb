6abe3328270a4924f0cbfcab09c91d7e8aedc39a
xaphaniafox <f.hosseini64@gmail.com>
2023-04-06 03:16:18 +0300

    hardcoding the data of cards instead of using API (prctice sake)

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,28 @@ import './style.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import data from './data'
 
 function App() {
+
+  const cardItems = data.map(item => {
+    return(
+      <Card
+        img = {item.coverImg}
+        rating = {item.stats.rating}
+        reviewCount = {item.stats.reviewCount}
+        location = {item.location}
+        title = {item.title}
+        price = {item.price}
+      />
+    )
+  })
 
   return (
     <>
       <Navbar />
       <Hero />
-      <Card />
+      {cardItems}
     </>
   )
 }
