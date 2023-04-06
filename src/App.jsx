44b63ff08a341da1788c7218913ba053caa16ed0
xaphaniafox <f.hosseini64@gmail.com>
2023-04-06 03:35:31 +0300

    adding the key property to get rid of the warning

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   const cardItems = data.map(item => {
     return(
       <Card
+        key = {item.id}
         img = {item.coverImg}
         rating = {item.stats.rating}
         reviewCount = {item.stats.reviewCount}
