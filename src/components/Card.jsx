ed642dae998d93fda4d79b3b7d3f921452f48f77
xaphaniafox <f.hosseini64@gmail.com>
2023-04-06 16:25:27 +0300

    cleaning up the Card component properties

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
 const Card = (props) => {
 
   let badgeText
-  if (props.openSpots === 0)
+  if (props.item.openSpots === 0)
   {
     badgeText = "SOLD OUT"
-  } else if (props.location === "Online")
+  } else if (props.item.location === "Online")
    {
     badgeText = "ONLINE"
    }
@@ -12,15 +12,15 @@ const Card = (props) => {
   return (
     <div className="card">
       {badgeText && <div className="card--badge">{badgeText}</div>}
-      <img src={`src/images/${props.img}`} className="card--image" />
+      <img src={`src/images/${props.item.coverImg}`} className="card--image" />
       <div className="card--stats">
         <img src='src/images/star.png' className="card-star" />
-        <span>{props.rating} </span>
-        <span className="gray">({props.reviewCount}) . </span>
-        <span className="gray">{props.location}</span>
+        <span>{props.item.stats.rating} </span>
+        <span className="gray">({props.item.stats.reviewCount}) . </span>
+        <span className="gray">{props.item.location}</span>
       </div>
-      <p>{props.title}</p>
-      <p><span className="bold">From ${props.price}</span> / person</p>
+      <p>{props.item.title}</p>
+      <p><span className="bold">From ${props.item.price}</span> / person</p>
     </div>
   )
 }
